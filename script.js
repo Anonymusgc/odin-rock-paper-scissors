b1c483ed8e6ec80bd4c459c8e7ca4be5a266c3b0
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-01-25 19:44:58 +0100

    Add function for player choice

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 "use strict";
 
-// const playerChoice = prompt("");
+const getPlayerChoice = () => {
+  let playerChoice = prompt("");
+  playerChoice = playerChoice.trim().toLowerCase();
+  console.log(playerChoice);
+
+  return playerChoice;
+};
 
 const getComputerChoice = () => {
   const randomChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
@@ -8,16 +14,25 @@ const getComputerChoice = () => {
   let computerChoice = "";
   switch (randomChoice) {
     case 1:
-      computerChoice = "Rock";
+      computerChoice = "rock";
       break;
     case 2:
-      computerChoice = "Paper";
+      computerChoice = "paper";
       break;
     case 3:
-      computerChoice = "Scissors";
+      computerChoice = "scissors";
       break;
   }
   console.log(computerChoice);
   return computerChoice;
 };
-getComputerChoice();
+
+const playRound = (playerChoice, computerChoice) => {
+  // let result = "";
+
+  if (playerChoice == "rock" && computerChoice == "rock") {
+    return playRound(getPlayerChoice(), getComputerChoice());
+  } else if (playerChoice == "rock" && computerChoice == "paper") {
+    return;
+  }
+};
